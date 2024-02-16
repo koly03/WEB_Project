103c760fc973431825b5908d066cfaea88cb00b7
koly03 <kol.y0363@gmail.com>
2024-02-16 17:08:14 +0100

    Our works slider almost finished

--- js/script1.js
+++ js/script1.js
@@ -5,6 +5,31 @@ const initSlider = () => {
     const imageList = document.querySelector(".ow-content .ow-content__slider");
     const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
 
+
+    scrollbarThumb.addEventListener("mousedown", (e) => {
+        const startX = e.clientX;
+        const thumbPosition = scrollbarThumb.offsetLeft;
+
+        const handleMouseMove = (e) => {
+            const deltaX = e.clientX - startX;
+            const newThumbPosition = thumbPosition + deltaX;
+            const maxThumbPosition = slideScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetLeft
+            const boundedPosition = Math.max(0,Math.min(maxThumbPosition, newThumbPosition));
+            const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+
+            scrollbarThumb.style.left = `${boundedPosition}px`;
+            imageList.scrollLeft = scrollPosition;
+        }
+
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove);
+            document.removeEventListener("mouseup", handleMouseUp);
+        }
+
+        document.addEventListener("mousemove", handleMouseMove);
+        document.addEventListener("mouseup", handleMouseUp);
+    })
+
     slideButtons.forEach(button => {
         button.addEventListener("click", () => {
             const direction = button.id === "prev-slide" ? -1 : 1;
