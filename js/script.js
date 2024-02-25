5660b513074b4aa6862b48161aa2432e6f113a08
koly03 <kol.y0363@gmail.com>
2024-02-25 21:14:19 +0100

    Burger menu added

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// const primaryNav = document.querySelector('.primary-navigation');
+// const navToggle = document.querySelector('.mobile-nav-toggle');
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Получаем ссылку на кнопку и список навигации
+    var mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+    var primaryNavigation = document.querySelector('.primary-navigation');
+
+    // При клике на кнопку
+    mobileNavToggle.addEventListener('click', function() {
+        // Проверяем, открыто ли меню навигации
+        var isExpanded = mobileNavToggle.getAttribute('aria-expanded') === 'true';
+
+        // Меняем состояние кнопки на противоположное
+        mobileNavToggle.setAttribute('aria-expanded', !isExpanded);
+
+        // Переключаем видимость списка навигации
+        if (isExpanded) {
+            // Если меню открыто, закрываем его
+            primaryNavigation.style.display = 'block';
+        } else {
+            // Если меню закрыто, открываем его
+            primaryNavigation.style.display = 'none';
+        }
+    });
+});
